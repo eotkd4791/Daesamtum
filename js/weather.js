86c76dcc99d9a7e13c6f52f545b00ae74ddb8d3d
stef <eotkd4791@gmail.com>
2019-11-26 18:40:15 +0900

    Update weather.js

--- js/weather.js
+++ js/weather.js
@@ -11,6 +11,7 @@ function getWeather(lat, lon){
     ).then(function(response){
         return response.json();
     }).then(function(json){
+        console.log(json);
         const temperature = json.main.temp;
         const place = json.name;
         weather.innerHTML = `${temperature}°C  @${place}`;
